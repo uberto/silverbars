037cbee0fee7bac8dd5a1ccc176b76573b8d514e
Uberto Barbini <uberto.gama@gmail.com>
2018-08-26 20:32:31 +0100

    Initial commit

---
+++ src/test/kotlin/com/gamasoft/silverbars/LiveBoardTest.kt
@@ -0,0 +1,13 @@
+package com.gamasoft.silverbars
+
+class LiveBoardTest {
+//
+//    - a) SELL: 3.5 kg for £306 [user1]
+//    - b) SELL: 1.2 kg for £310 [user2]
+//    - c) SELL: 1.5 kg for £307 [user3]
+//    - d) SELL: 2.0 kg for £306 [user4]
+//    Our ‘Live Order Board’ should provide us the following summary information:
+//    - 5.5 kg for £306 // order a + order d
+//    - 1.5 kg for £307 // order c
+//    - 1.2 kg for £310 // order b
+}
